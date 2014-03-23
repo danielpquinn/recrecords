7ca55271dfdc31be44c67f2032e45850b658d4b5
Dan Quinn <danthemilk@gmail.com>
2014-03-23 00:19:19 +0000

    Used minified versions of scripts in build

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp'),
 
 gulp.task('compress', function () {
   gulp.src([
-    'public/bower_components/jquery/jquery.js',
-    'public/bower_components/angular/angular.js',
-    'public/bower_components/angular-animate/angular-animate.js',
-    'public/bower_components/angular-route/angular-route.js',
-    'public/bower_components/soundmanager/script/soundmanager2.js',
+    'public/bower_components/jquery/jquery.min.js',
+    'public/bower_components/angular/angular.min.js',
+    'public/bower_components/angular-animate/angular-animate.min.js',
+    'public/bower_components/angular-route/angular-route.min.js',
+    'public/bower_components/soundmanager/script/soundmanager2-jsmin.js',
     'public/bower_components/underscore/underscore.js',
     'public/js/controllers.js',
     'public/js/directives.js',
