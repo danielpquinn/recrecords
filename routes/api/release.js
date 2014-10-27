df622d90fbb9308daee1f3d607d399b2ac420e1d
danielpquinn <dan@danquinndesign.com>
2014-10-26 20:24:39 -0700

    Added artist name to player UI

--- routes/api/release.js
+++ routes/api/release.js
@@ -1,7 +1,19 @@
 var keystone = require('keystone');
+var Q = require('q');
 
 exports = module.exports = function (req, res) {
-  keystone.list('Release').model.findOne({ slug: req.params.slug }).populate('tracks artist genres').exec(function (err, results) {
-    res.json(results);
+  var artistGets = [];
+
+  keystone.list('Release').model.findOne({ slug: req.params.slug }).populate('tracks artist genres').lean().exec(function (err, release) {
+    release.tracks.forEach(function (track) {
+      artistGets.push(keystone.list('Artist').model.findOne({ _id: track.artist }).exec());
+    });
+    Q.all(artistGets)
+      .then(function (artists) {
+        release.tracks.forEach(function (track, i) {
+          track.artist = artists[i];
+        });
+        res.json(release);
+      });
   });
 };
